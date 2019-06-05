814172eef4262a8d59bd05d8c8cd1ce58c8e7817
Frederic Reinhardt <fredericreinhardt7@gmail.com>
2019-06-05 20:19:03 +0200

    removed unnecessary code, optimized the file save path, commented everything in english, removed / translated the german description

--- miniOCR.py
+++ miniOCR.py
@@ -1,54 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pyperclip #used to save Var result into clipboard
-import pytesseract
-import webbrowser
-import PIL.ImageGrab
-from PIL import Image, ImageEnhance, ImageFilter
-import requests
-from difflib import SequenceMatcher
-import difflib
-import string
-import time
-import os
+import pyperclip 	#to save data into clipboard
+import pytesseract	#makes ocr possible
+import PIL.ImageGrab	#grabs image from clipboard
+from PIL import Image, ImageEnhance, ImageFilter #to use / interact with images
+import time #used for the "waiting.." process
+import os #for OS paths
 from PIL import ImageGrab
+import subprocess #to call a snipping tool // will be obsolete in a future version of this
 
-import re #regex
-import os.path
-import time
-
-#function cleanhtml - erases html tags from the result
-def cleanhtml(raw_html):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', raw_html)
-  return cleantext
-
-
-import subprocess
 subprocess.call(['SnippingTool.exe']) #SnippingTool of your choice! I recommend SnippingToolPlus, because Windows integrated SnippingTool will be obsolete soon.
 
-filepath = 'C:\\Users\\YOURNAME\\tmpfile.png' #Place where the tmpfile should get saved
-otherfile = 'C:\\Users\\YOURNAME\\somefile.png' #Place where the tmpfile should get saved
+filepath = '.\\tmpfile.png' #Place where the tmpfile should get saved, default: same directory
+otherfile = '.\\recognizeMe.png' #Place where the tmpfile should get saved, default: same directory
 
 while not os.path.exists(filepath):
-   print("waiting..")
+   print("waiting for image..")
    time.sleep(1)
    im = ImageGrab.grabclipboard()
    if im:
       im.save('tmpfile.png','PNG')
 
-if os.path.isfile(filepath):
+if os.path.isfile(filepath): #checks if a file got created
    os.rename( filepath , otherfile)
-
-   FrageText = pytesseract.image_to_string(Image.open("somefile.png"),lang="deu") #Befehl der das Bild in Text verwandelt (Bild wird geöffnet (Angabe des Pfades), Sprachpaket / Trainingsdaten für pytesseract = arial)
-   FrageText = FrageText.replace('\n', ' ') # Ersetzt den Zeichenumbruch mit einem Leerzeichen 
-
-   pyperclip.copy(FrageText) #Kopiert erkannten Text in die Zwischenablage
-
-   print(FrageText)
-
-   os.remove(otherfile)
+   recognizedText = pytesseract.image_to_string(Image.open("recognizeMe.png"),lang="deu") #converts the image to text via optical text recognition (opens the image (path), languagepack for pytesseract)
+   recognizedText = recognizedText.replace('\n', ' ') #replaces a line-break with space
+   pyperclip.copy(recognizedText) #copies recognized text to the clipboard
+   os.remove(otherfile) #removes the saved file
    
 else:
    raise ValueError("%s isn't a file!" % filepath)
